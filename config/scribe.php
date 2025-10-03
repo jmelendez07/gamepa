7555e921fb7f5e5d1379d1eab7ad30078c5e702c
jose <josmelendezramos123@gmail.com>
2025-10-03 00:59:22 -0500

    Add: configuración adicional de scribe agregada

--- config/scribe.php
+++ config/scribe.php
@@ -24,7 +24,7 @@ return [
 
     // The base URL displayed in the docs.
     // If you're using `laravel` type, you can set this to a dynamic string, like '{{ config("app.tenant_url") }}' to get a dynamic base URL.
-    'base_url' => config("app.url"),
+    'base_url' => env('OPEN_API_BASE_URL', 'https://math-g-pre-production.up.railway.app'),
 
     // Routes to include in the docs
     'routes' => [
